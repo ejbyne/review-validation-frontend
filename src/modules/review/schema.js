e053bedb083a8a8de2714ba5151854c1670d01e1
Edward Byne <edward.byne@codecentric.de>
2018-04-18 08:23:25 +0200

    Schema fixes

--- src/modules/review/schema.js
+++ src/modules/review/schema.js
@@ -1,19 +1,31 @@
 const Joi = require('joi');
 
 const reviewSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  firstName: Joi.string()
+    .empty('')
+    .required(),
+  lastName: Joi.string()
+    .empty('')
+    .required(),
   email: Joi.string()
+    .empty('')
     .regex(/^.+@.+$/)
     .required(),
   score: Joi.number()
     .integer()
+    .empty('')
     .min(0)
     .max(10)
     .required(),
-  comment: Joi.string().max(250),
-  date: Joi.date().required(),
-  willComeAgain: Joi.string().valid(['yes', 'no', 'maybe'])
+  comment: Joi.string()
+    .empty('')
+    .max(250),
+  date: Joi.date()
+    .empty('')
+    .required(),
+  willComeAgain: Joi.string()
+    .empty('')
+    .valid(['yes', 'no', 'maybe'])
 }).label('Review');
 
 const reviewCollectionSchema = Joi.array()
